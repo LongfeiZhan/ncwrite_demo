c533c0fe898bde5f9faaa8f702c951b5b47e6204
Longfei Zhan <lf.zhan@foxmail.com>
2020-08-23 23:36:18 +0800

    Update ncwrite_demo.m

--- ncwrite_demo.m
+++ ncwrite_demo.m
@@ -1,7 +1,7 @@
 clear;
 clc;
 
-% ????
+% creat variable
 nccreate('myncfile.nc','data',...
     'Dimensions', {'x',3,'y',6,'z',100},...
     'FillValue','disable');
@@ -9,10 +9,10 @@ nccreate('myncfile.nc','lon','Dimensions',{'x',3});
 nccreate('myncfile.nc','lat','Dimensions',{'y',6});
 nccreate('myncfile.nc','time','Dimensions',{'z',100});
 
-% ????
+% put data to variable
 ncwrite('myncfile.nc','lon',zeros(3,1));
 ncwrite('myncfile.nc','lat',zeros(6,1));
 ncwrite('myncfile.nc','data',zeros(3,6,100));
 
-% ????
+% check whether success
 info = ncinfo('myncfile.nc');
